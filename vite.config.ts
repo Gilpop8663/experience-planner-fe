129d2fcc3f0143cc8341343f694cf7e8d4ab2ca4
gilpop8663 <wolfye@naver.com>
2024-09-17 05:07:06 +0900

    chore: base 추가

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: "/experience-planner-fe/",
+    base: "",
     define: {
       "process.env.VITE_DB_URL": JSON.stringify(env.VITE_DB_URL),
     },
